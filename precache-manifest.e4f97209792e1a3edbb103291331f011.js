108466de581d039c7ddf926affec5d959c5371b7
Anton <antoncorbn@gmx.com>
2020-05-19 22:22:15 +0200

    version 2

---
+++ precache-manifest.e4f97209792e1a3edbb103291331f011.js
@@ -0,0 +1,366 @@
+self.__precacheManifest = [
+  {
+    "revision": "4392f303a1649236fb74dcf899bc397d",
+    "url": "/static/media/icon_curr_won.4392f303.png"
+  },
+  {
+    "revision": "10f709264a7ed4dd2a7d",
+    "url": "/static/js/app.5d35e5de.chunk.js"
+  },
+  {
+    "revision": "26d2fdf81898ee93d96e6e8ac5311cc1",
+    "url": "/static/media/icon_curr_southafricanrand.26d2fdf8.png"
+  },
+  {
+    "revision": "6270e058976bc5b5a225f33f6ce77c85",
+    "url": "/static/media/icon_coin_peg.6270e058.png"
+  },
+  {
+    "revision": "3e1975c47e08baba048dc2de0814b874",
+    "url": "/static/media/transaction-receive-icon.3e1975c4.png"
+  },
+  {
+    "revision": "c3fbb1ecb6dd2cff3fff41979c762f66",
+    "url": "/static/media/transaction-send-icon.c3fbb1ec.png"
+  },
+  {
+    "revision": "11edcaa8728bf9e8d135e5aa54da26ee",
+    "url": "/static/media/profile-image.11edcaa8.png"
+  },
+  {
+    "revision": "b83d88349203c6c6947e19f4b9236fa8",
+    "url": "/static/media/send-icon.b83d8834.png"
+  },
+  {
+    "revision": "98424cd2bfedc49503ca0a8ecba72024",
+    "url": "/static/media/topup-icon.98424cd2.png"
+  },
+  {
+    "revision": "6165c9d7a2e729ba57b23dd93add5366",
+    "url": "/static/media/back-icon-mask.6165c9d7.png"
+  },
+  {
+    "revision": "83305b352b72dae091a56d93c198604a",
+    "url": "/static/media/gradient.83305b35.png"
+  },
+  {
+    "revision": "39ba4a522725c7592b853e52a0640156",
+    "url": "/static/media/icon_curr_ausdollar.39ba4a52.png"
+  },
+  {
+    "revision": "7a5be2f21a4039a8c7a1cd955dff2d51",
+    "url": "/static/media/icon_coin_eos.7a5be2f2.png"
+  },
+  {
+    "revision": "d8b92d28a80aa6aa363f23047581366d",
+    "url": "/static/media/receive-icon.d8b92d28.png"
+  },
+  {
+    "revision": "557f36e344355a969316506936ab5539",
+    "url": "/static/media/convert-icon.557f36e3.png"
+  },
+  {
+    "revision": "4112d9b6a7aeb58e2564bcdd5cca9558",
+    "url": "/static/media/logo-white.4112d9b6.png"
+  },
+  {
+    "revision": "dcd352d14fb48a94c986c9ddec206ef8",
+    "url": "/static/media/touch-icon.dcd352d1.png"
+  },
+  {
+    "revision": "a146664ecf6f9c269ab11ae8b7582f93",
+    "url": "/static/media/lock-icon.a146664e.png"
+  },
+  {
+    "revision": "d26272639dcb5ab69f4de12dcb769665",
+    "url": "/static/media/user-icon.d2627263.png"
+  },
+  {
+    "revision": "a0381efbb3ce00d1784ee685adf2adad",
+    "url": "/static/media/help-icon.a0381efb.png"
+  },
+  {
+    "revision": "14830846a6e10f9e3ca4815e15491fd3",
+    "url": "/static/media/user-icon.14830846.png"
+  },
+  {
+    "revision": "bd2c7459b8d11b19ef1df785a775c7c0",
+    "url": "/static/media/pin-icon.bd2c7459.png"
+  },
+  {
+    "revision": "0909734d40789fe39255d38f988f2ec3",
+    "url": "/static/media/backup-wallet-icon.0909734d.png"
+  },
+  {
+    "revision": "10dc3cbcaeb561193c73f7f33f78c249",
+    "url": "/static/media/login-pin-icon.10dc3cbc.png"
+  },
+  {
+    "revision": "cda5154516c5612b46bc6f64b27c62af",
+    "url": "/static/media/notification-icon.cda51545.png"
+  },
+  {
+    "revision": "607286332dc878298af560999a96aa64",
+    "url": "/static/media/sound-icon.60728633.png"
+  },
+  {
+    "revision": "c41784f570147ff58eaccb9d97529f2a",
+    "url": "/static/media/language-icon.c41784f5.png"
+  },
+  {
+    "revision": "118583bea6b81d76a63315b279d4125c",
+    "url": "/static/media/currency-icon.118583be.png"
+  },
+  {
+    "revision": "42e787dd7371d093de4a5b11174cc714",
+    "url": "/static/media/power-icon.42e787dd.png"
+  },
+  {
+    "revision": "39a85596e7cb8e1058f8f136beef1060",
+    "url": "/static/media/gradient-footer.39a85596.png"
+  },
+  {
+    "revision": "b1b5bac33c4cc32aece460b046e2b5d6",
+    "url": "/static/media/checkmark.b1b5bac3.png"
+  },
+  {
+    "revision": "678a91244f5e2a0289afe285769377c1",
+    "url": "/static/media/add-contact-icon.678a9124.png"
+  },
+  {
+    "revision": "029188dfe06ecc3b8564153358b9e8f3",
+    "url": "/static/media/share-icon.029188df.png"
+  },
+  {
+    "revision": "f5b3c9f3d232d3d47b7c60786da75ea9",
+    "url": "/static/media/logo-white.f5b3c9f3.png"
+  },
+  {
+    "revision": "a6467fd222a1948a865309ed1f2be85f",
+    "url": "/static/media/icon_coin_pbtc.a6467fd2.png"
+  },
+  {
+    "revision": "3e37f9f785f5212ebda9e105b1115675",
+    "url": "/static/media/icon_curr_usdollar.3e37f9f7.png"
+  },
+  {
+    "revision": "78f12cd6228cfe3a11f4a0d936f54750",
+    "url": "/static/media/icon_coin_factom.78f12cd6.png"
+  },
+  {
+    "revision": "3d44a55fbde6ede71c64e90f6e113479",
+    "url": "/static/media/icon_com_gold.3d44a55f.png"
+  },
+  {
+    "revision": "3bfe6b7328ef2383a4cd9e67bade9f75",
+    "url": "/static/media/icon_coin_peth.3bfe6b73.png"
+  },
+  {
+    "revision": "a78b4b6178097f544d29a210a9772ed0",
+    "url": "/static/media/icon_coin_tezos.a78b4b61.png"
+  },
+  {
+    "revision": "5999da16828f2d3465f106d541523467",
+    "url": "/static/media/icon_coin_decred.5999da16.png"
+  },
+  {
+    "revision": "265b5b7fa90ccda5fa09fe03e55bee47",
+    "url": "/static/media/icon_coin_bch.265b5b7f.png"
+  },
+  {
+    "revision": "5726ffd7666bb6f2cbbc6c46ed8c5d4f",
+    "url": "/static/media/icon_coin_dash.5726ffd7.png"
+  },
+  {
+    "revision": "6b41cb5cf57878b4766f754225eda4b5",
+    "url": "/static/media/icon_coin_chainlink.6b41cb5c.png"
+  },
+  {
+    "revision": "eba8c7677e2b9be7d6ee74a20f25a561",
+    "url": "/static/media/icon_com_silver.eba8c767.png"
+  },
+  {
+    "revision": "328ec0abaf4b8f214d7f",
+    "url": "/static/js/runtime~app.b3244af7.js"
+  },
+  {
+    "revision": "534dc01485754efb48dde7df55f276ca",
+    "url": "/static/media/icon_coin_binancecoin.534dc014.png"
+  },
+  {
+    "revision": "6aa91765163a1109376d17e705f9db6e",
+    "url": "/static/media/icon_coin_atom.6aa91765.png"
+  },
+  {
+    "revision": "51c8c03f01a3f8168530621589cf4258",
+    "url": "/static/media/icon_coin_zcash.51c8c03f.png"
+  },
+  {
+    "revision": "25c11abbff25c43a1ef26637e5b4d14e",
+    "url": "/static/media/icon_coin_cardano.25c11abb.png"
+  },
+  {
+    "revision": "391dff92e80eed420f5901a143666655",
+    "url": "/static/media/icon_coin_litecoin.391dff92.png"
+  },
+  {
+    "revision": "7baaea84799755bdabab6cd9de2a5336",
+    "url": "/static/media/icon_coin_monero.7baaea84.png"
+  },
+  {
+    "revision": "47b0b2990d0a8426fee9e34dd6378400",
+    "url": "/static/media/icon_coin_stellar.47b0b299.png"
+  },
+  {
+    "revision": "6086dfe76a1ee2aaafe2b18a4f91a143",
+    "url": "/static/media/icon_coin_raven.6086dfe7.png"
+  },
+  {
+    "revision": "4c05af1ce706dc4bdc90449fd392a688",
+    "url": "/static/media/icon_curr_euro.4c05af1c.png"
+  },
+  {
+    "revision": "222740d12e20c96003d0ae611490f00a",
+    "url": "/static/media/icon_curr_yuan.222740d1.png"
+  },
+  {
+    "revision": "a832d2bba3c89cf2343c53f70c770e94",
+    "url": "/static/media/icon_curr_pound.a832d2bb.png"
+  },
+  {
+    "revision": "38a2f80acc451d0351a1364651f39b0a",
+    "url": "/static/media/icon_curr_swissfranc.38a2f80a.png"
+  },
+  {
+    "revision": "ab6c7cbde2c398f83e913e54b7a8cac2",
+    "url": "/static/media/icon_curr_peso.ab6c7cbd.png"
+  },
+  {
+    "revision": "9e1dddbcb4582ab4edcb965fb56e69fe",
+    "url": "/static/media/icon_curr_hkdollar.9e1dddbc.png"
+  },
+  {
+    "revision": "47d5070039756d17e450c855a2d19b37",
+    "url": "/static/media/icon_curr_philepinepeso.47d50700.png"
+  },
+  {
+    "revision": "54f74d694f50024b214f116e622df0d4",
+    "url": "/static/media/icon_curr_yen.54f74d69.png"
+  },
+  {
+    "revision": "fb0728253ef34f5e033e33204c0708ee",
+    "url": "/static/media/icon_curr_rupee.fb072825.png"
+  },
+  {
+    "revision": "c586b33c090b9bede7c3eea3e8fac03b",
+    "url": "/static/media/icon_curr_candollar.c586b33c.png"
+  },
+  {
+    "revision": "509ae014a47e5793350d3750c89823dd",
+    "url": "/static/media/icon_curr_singaporedollar.509ae014.png"
+  },
+  {
+    "revision": "65cfadb7f1c236315722b74a1a5c0e43",
+    "url": "/static/media/icon_curr_brazilreal.65cfadb7.png"
+  },
+  {
+    "revision": "2a8bd66cae7f891d552c3356a4a2ac4a",
+    "url": "/static/media/icon_curr_newzealanddollar.2a8bd66c.png"
+  },
+  {
+    "revision": "2a7d00e6fa0b2f368d8fb1b4813fbcf7",
+    "url": "/static/media/icon_curr_norwegiankrone.2a7d00e6.png"
+  },
+  {
+    "revision": "23915501111442082b0e01b02af810b5",
+    "url": "/static/media/icon_curr_ruble.23915501.png"
+  },
+  {
+    "revision": "0ba2bd8f89343eca7e668e8fa430e9cb",
+    "url": "/static/media/icon_curr_swedishkrona.0ba2bd8f.png"
+  },
+  {
+    "revision": "d3e2eaebc7a385a6cf7690022245f002",
+    "url": "/static/media/icon_coin_basicattentiontoken.d3e2eaeb.png"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "24701a16300b95469ff0",
+    "url": "/static/js/2.dd1c6eec.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "30cff8e8cb1c1e8cc2ae553cb460e455",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d9ce0fedc05eb45de26520f221f50f9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0510835f590946831073815cbda2eec0",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "36f8d1ae07158daf26c1f992edf952b5",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "24a5b3973e5e5f6728dd7db78188e52d",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  }
+];
